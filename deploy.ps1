5bb46f2c6c8ce52654e4be2c962a33b2dae30694
Zach Bonham <zachbonham@gmail.com>
2016-09-08 17:57:21 -0500

    removed credential for now.  adding scheduled task

--- deploy.ps1
+++ deploy.ps1
@@ -10,6 +10,13 @@ Requirements
     RISK: PowerShell DSC Roles and Features requires server SKU's.  Non servers targets are a risk.
 #>
 
+cls
+
+Write-Host "$([char]0x00A9) 2016, WatchGuard Video.  All rights reserved."
+Write-Host ""
+Write-Host "Package deployment starting ..."
+
+
 . .\DeployDependencies.ps1
 . .\DeployContext.ps1
 . .\SqlPackager.ps1
@@ -17,18 +24,20 @@ Requirements
 # push down 
 Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds" -Name "ConsolePrompting" -Value $True
 
-
 [xml]$environment = get-content .\localhost.environment
-
 
 $dc = new-object DeployContext
 
+<#
 $credential = Get-Credential -Username $Env:Userdomain\$Env:Username -m "The credential that will be used to administer WatchGuard Video applications"
 $dc.DeployCredential = $credential
+#>
 
 Write-Host "Deploying database projects"
 
-Deploy-WgvAllDatabases $dc
+Deploy-WgvAllDatabases -deployContext $dc
+
+exit
 
 .\DashboardAppRole -deployContext $dc | out-null
 
@@ -37,4 +46,4 @@ Start-DscConfiguration -path .\DashboardAppRole -wait -force
 Write-Host "DSC configuration complete"
 
 Write-Host ""
-Write-Host "Deployment Complete.  Have a nice day."
+Write-Host "Package deployment omplete.  Have a nice day."
